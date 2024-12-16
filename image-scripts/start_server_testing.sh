1410b369788d71ef21af30a4f7268f6452de387a
Spafbi <Spafbi@users.noreply.github.com>
2024-12-16 07:42:28 +0000

    Add testing files

---
+++ image-scripts/start_server_testing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Start X virtual framebuffer
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export XDG_RUNTIME_DIR=$(mktemp -d)
+Xvfb :0 -screen 0 1280x1024x16 &
+## Start VNC server
+echo "Starting VNC server..."
+x11vnc -display :0 -forever -passwd pcKoyxaA5wYguW3r -listen 0.0.0.0 -no6 -xkb -rfbport 5900 -quiet &
+while true; do
+wine Bin64_dedicated/MiscreatedServer.exe +sv_maxplayers 50 +map islands +http_startserver
+WINE_PID=$!
+  while [ ! -f /app/server.log ]; do
+    sleep 1
+  done
+  tail -n100 -f /app/server.log &
+  TAIL_PID=$!
+  wait $WINE_PID
+  kill $TAIL_PID
+  sleep 10
+done
