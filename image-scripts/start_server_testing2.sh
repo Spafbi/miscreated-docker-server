59319d4584737b8dc2330a3934c9c877bb2b85da
Spafbi <Spafbi@users.noreply.github.com>
2024-12-18 08:22:21 +0000

    Testing tweaks

---
+++ image-scripts/start_server_testing2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while true; do
+  export WINEDLLOVERRIDES="mscoree,mshtml="
+  # Start X virtual framebuffer
+  export XDG_RUNTIME_DIR=$(mktemp -d)
+  /usr/bin/xvfb-run --server-args="-screen 0 1280x1024x24" /usr/bin/x11vnc -forever -passwd nopass01 -create
+  XVFB_PID=$!
+  /etc/init.d/xrdp start
+  XRDP_PID=$!
+  wine Bin64_dedicated/MiscreatedServer.exe +sv_maxplayers 50 +map islands +http_startserver
+  WINE_PID=$!
+  while [ ! -f /app/server.log ]; do
+    sleep 1
+  done
+  tail -n1000 -f /app/server.log &
+  TAIL_PID=$!
+  wait $WINE_PID
+  kill $TAIL_PID $XVFB_PID $XRDP_PID
+  sleep 10
+done
