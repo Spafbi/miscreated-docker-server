0a5981155fad0416ca172b90a930749a80ad1b87
Spafbi <Spafbi@users.noreply.github.com>
2024-12-18 11:42:13 +0000

    Testing tweaks

--- image-scripts/start_server_testing2.sh
+++ image-scripts/start_server_testing2.sh
@@ -7,12 +7,13 @@ while true; do
   export XDG_RUNTIME_DIR=$(mktemp -d)
   # /usr/bin/xvfb-run --server-args="-screen 0 1280x1024x24" /usr/bin/x11vnc -forever -passwd nopass01 -create
   export DISPLAY=:0
-  /usr/bin/xvfb-run --server-args="-screen 0 1280x1024x24"
+  /usr/bin/xvfb-run --server-args="-screen 0 1280x1024x24"Xvfb :0 -screen 0 1280x1024x24 &
+  XVFB_PID=$!
   # Wait to ensure Xvfb is up
   sleep 5
   # Start XRDP
   /etc/init.d/xrdp start
-  XVFB_PID=$!
+  XRDP_PID=$!
   # /etc/init.d/xrdp start
   # XRDP_PID=$!
   wine Bin64_dedicated/MiscreatedServer.exe +sv_maxplayers 50 +map islands +http_startserver
@@ -23,9 +24,9 @@ while true; do
   tail -n1000 -f /app/server.log &
   TAIL_PID=$!
   wait $WINE_PID
-  kill $TAIL_PID $XRDP_PID
+  # kill $TAIL_PID $XRDP_PID
+  kill $TAIL_PID $XVFB_PID $XRDP_PID
   # Clean up the temporary directory
   rm -rf $XDG_RUNTIME_DIR
-  # kill $TAIL_PID $XVFB_PID $XRDP_PID
   sleep 10
 done
